eebae0c01304ed16b4b6c0282968b05f0d7db5d2
Pano Papadatos <pano.papadatos@publicrelay.com>
2020-05-24 09:08:26 -0400

    adding cat drawing optimizations

--- src/redux/features/cursor/cursorSlice.js
+++ src/redux/features/cursor/cursorSlice.js
@@ -1,13 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { createSelector } from "@reduxjs/toolkit";
 
+const name = "cursor";
+
 const cursorSlice = createSlice({
-  name: "cursor",
+  name: name,
   initialState: {
     x: 0,
-    y: 0
+    y: 0,
+    isDrawing: false
   },
   reducers: {
+    setIsDrawing(state) {
+      state.isDrawing = true;
+    },
+    setIsNotDrawing(state) {
+      state.isDrawing = false;
+    },
     setPosition(state, action) {
       const { x, y } = action.payload;
       state.x = x;
@@ -25,13 +34,24 @@ const cursorSlice = createSlice({
 });
 
 export const selectCursorPosition = createSelector(
-  state => state.cursor,
+  state => state[name],
   cursor => ({
     x: cursor.x,
     y: cursor.y
   })
 );
 
-export const { setPosition, setPositionX, setPositionY } = cursorSlice.actions;
+export const selectIsDrawing = createSelector(
+  state => state[name],
+  cursor => cursor.isDrawing
+);
+
+export const {
+  setPosition,
+  setPositionX,
+  setPositionY,
+  setIsNotDrawing,
+  setIsDrawing
+} = cursorSlice.actions;
 
 export default cursorSlice.reducer;
